cf01f5f65a6a6e1165ee1cbda92c1e177063ac84
DmitryVozny <dmitry.vozny@gmail.com>
2023-08-22 15:19:37 +0300

    opt

--- js/swiper.js
+++ js/swiper.js
@@ -1,13 +1,1 @@
-const swiper = new Swiper('.swiper-container', {
-  // Цикличность
-  loop: true,
-
-  // Пагинация
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-  },
-  a11y: {
-    paginationBulletMessage: 'Название слайда {{index}}',
-  }
-});
+function swiperHero(){new Swiper(".swiper-container",{loop:!0,pagination:{el:".swiper-pagination",clickable:!0},a11y:{paginationBulletMessage:"Название слайда {{index}}"}})}swiperHero();
